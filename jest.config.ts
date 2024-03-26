5f04bb9a735e77d61894e7bd83fd8252d0bec028
javierhersan <javierhersan1999@gmail.com>
2024-03-26 20:39:38 +0100

    test(config): Remove test coverage threshold

--- jest.config.ts
+++ jest.config.ts
@@ -8,12 +8,5 @@ export default {
 	modulePathIgnorePatterns: ['./dist/'],
 	coveragePathIgnorePatterns: [],
 	collectCoverageFrom: ['./src/**/*.ts', '!./src/**/*.test.ts'],
-	coverageThreshold: {
-		global: {
-			branches: 100,
-			functions: 100,
-			lines: 100,
-			statements: 100,
-		},
-	},
+	coverageThreshold: {},
 }
