277a14494ac1600b30f07d7120a85cb7a1b42d3f
Javier Hernández Sánchez <javierhersan1999@gmail.com>
2024-04-01 11:49:24 +0200

    feature(cryptography)!: CryptoKey to String encoder/decoder and change API to accept string signatures
    BREAKING CHANGES

--- src/cryptography/cryptography.ts
+++ src/cryptography/cryptography.ts
@@ -1,4 +1,5 @@
 import { Keccak } from 'sha3'
+import type { IKeyPair } from '../models/IKeyPair'
 
 export class Cryptography {
 		
@@ -8,8 +9,8 @@ export class Cryptography {
 		return "0x"+hash.digest('hex')
 	}
 	
-	public static async generateSignatureKeyPair(): Promise<CryptoKeyPair> {
-		const keypair = await crypto.subtle.generateKey(
+	public static async generateSignatureKeyPair(): Promise<IKeyPair> {
+		const cryptoKeyPair = await crypto.subtle.generateKey(
 			{
 			  name: "ECDSA",
 			  namedCurve: "P-256",
@@ -17,29 +18,33 @@ export class Cryptography {
 			true,
 			["sign", "verify"]
 		  );
-		return keypair;
+		const keyPair: IKeyPair = {
+			publicKey: await this.publicKeyToString(cryptoKeyPair.publicKey),
+			privateKey: await this.privateKeyToString(cryptoKeyPair.privateKey),
+		}
+		return keyPair;
 	}
 
-	public static async sign(message: string, privateKey: CryptoKey): Promise<string> {
+	public static async sign(message: string, privateKey: string): Promise<string> {
 		try {
 			const hash = await this.hash(message)
 			const signature = await crypto.subtle.sign({
 				name: "ECDSA",
 				hash: "SHA-256",
-			  }, privateKey, this.stoab(hash))
+			  }, await this.stringToPrivateKey(privateKey), this.stoab(hash))
 			return this.abtos(signature)
 		} catch (err) {
 			throw Error()
 		}
 	}
 
-	public static async verify(message: string, signature: string, publicKey: CryptoKey): Promise<boolean> {
+	public static async verify(message: string, signature: string, publicKey: string): Promise<boolean> {
 		try {
 			const hash = await this.hash(message)
 			const verification = await crypto.subtle.verify({
 				name: "ECDSA",
 				hash: "SHA-256",
-			  }, publicKey, this.stoab(signature), this.stoab(hash))
+			  }, await this.stringToPublicKey(publicKey), this.stoab(signature), this.stoab(hash))
 			return verification
 		} catch (err) {
 			throw Error()
@@ -85,5 +90,31 @@ export class Cryptography {
 	  
 	public static stoab(str: string): ArrayBuffer {
 		return Uint8Array.from(str, x => x.charCodeAt(0)).buffer
+	}
+
+	public static async publicKeyToString(key: CryptoKey): Promise<string> {
+		return this.abtos(await crypto.subtle.exportKey('raw',key))
+	}
+	  
+	public static async stringToPublicKey(key: string): Promise<CryptoKey> {
+		return await crypto.subtle.importKey('raw', this.stoab(key), {
+			name: "ECDSA",
+			namedCurve: "P-256",
+		  },
+		  true,
+		  ["verify"]);
+	}
+
+	public static async privateKeyToString(key: CryptoKey): Promise<string> {
+		return this.abtos(await crypto.subtle.exportKey('pkcs8',key))
+	}
+	  
+	public static async stringToPrivateKey(key: string): Promise<CryptoKey> {
+		return await crypto.subtle.importKey('pkcs8', this.stoab(key), {
+			name: "ECDSA",
+			namedCurve: "P-256",
+		  },
+		  true,
+		  ["sign"]);
 	}
 }
