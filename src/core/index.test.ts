031e497d8aec9a962fd22d07a67b1611da9a371e
Javier Hernández Sánchez <javierhersan1999@gmail.com>
2025-01-19 23:30:15 +0100

    fix(core): fix core testing due to module rename

--- src/core/index.test.ts
+++ src/core/index.test.ts
@@ -1,35 +1,31 @@
-import { TrustNetEngine } from '.'
+import { Core } from '.'
 
 describe('Core module test suite', () => {
 	it('Hash Unit Test', async () => {
-		expect(await TrustNetEngine.hash('Message')).toEqual(
+		expect(await Core.hash('Message')).toEqual(
 			'0x9a59efbc471b53491c8038fd5d5fe3be0a229873302bafba90c19fbe7d7c7f35',
 		)
 	})
 
 	it('Signature e2e test', async () => {
-		const keyPair = await TrustNetEngine.generateSignatureKeyPair()
+		const keyPair = await Core.generateSignatureKeyPair()
 		const publicKey = keyPair.publicKey
 		const privateKey = keyPair.privateKey
 		const message = 'Message'
-		const signature = await TrustNetEngine.sign(message, privateKey)
-		const verification = await TrustNetEngine.verify(
-			message,
-			signature,
-			publicKey,
-		)
+		const signature = await Core.sign(message, privateKey)
+		const verification = await Core.verify(message, signature, publicKey)
 		expect(verification).toEqual(true)
 	})
 
 	it('Block Header Signature Unit Test', async () => {
-		const accountkeyPair = await TrustNetEngine.generateSignatureKeyPair()
+		const accountkeyPair = await Core.generateSignatureKeyPair()
 		const accountPublicKey = accountkeyPair.publicKey
 
-		const blockKeyPair = await TrustNetEngine.generateSignatureKeyPair()
+		const blockKeyPair = await Core.generateSignatureKeyPair()
 		const blockPublicKey = blockKeyPair.publicKey
 		const blockPrivateKey = blockKeyPair.privateKey
 
-		const unsignedblockHeader = await TrustNetEngine.generateBlockHeader(
+		const unsignedblockHeader = await Core.generateBlockHeader(
 			'Content',
 			accountPublicKey,
 			blockPublicKey,
@@ -38,53 +34,51 @@ describe('Core module test suite', () => {
 			1,
 		)
 
-		const signedBlockHeader = await TrustNetEngine.signBlockHeader(
+		const signedBlockHeader = await Core.signBlockHeader(
 			unsignedblockHeader,
 			blockPrivateKey,
 		)
 
-		expect(
-			await TrustNetEngine.verifyBlockHeaderSignature(signedBlockHeader),
-		).toEqual(true)
+		expect(await Core.verifyBlockHeaderSignature(signedBlockHeader)).toEqual(
+			true,
+		)
 	})
 
 	it('Seed Block Signature Unit Test', async () => {
-		const accountkeyPair = await TrustNetEngine.generateSignatureKeyPair()
+		const accountkeyPair = await Core.generateSignatureKeyPair()
 		const accountPublicKey = accountkeyPair.publicKey
 		const accountPrivateKey = accountkeyPair.privateKey
 
-		const blockKeyPair = await TrustNetEngine.generateSignatureKeyPair()
+		const blockKeyPair = await Core.generateSignatureKeyPair()
 		const blockPublicKey = blockKeyPair.publicKey
 
-		const unsignedSeedBlock = await TrustNetEngine.generateSeedBlock(
+		const unsignedSeedBlock = await Core.generateSeedBlock(
 			accountPublicKey,
 			blockPublicKey,
 			1,
 			1,
 		)
 
-		const signedSeedBlock = await TrustNetEngine.signSeedBlock(
+		const signedSeedBlock = await Core.signSeedBlock(
 			unsignedSeedBlock,
 			accountPrivateKey,
 		)
 
-		expect(
-			await TrustNetEngine.verifySeedBlockSignature(signedSeedBlock),
-		).toEqual(true)
+		expect(await Core.verifySeedBlockSignature(signedSeedBlock)).toEqual(true)
 	})
 
 	it('Block Unit Test', async () => {
-		const accountkeyPair = await TrustNetEngine.generateSignatureKeyPair()
+		const accountkeyPair = await Core.generateSignatureKeyPair()
 		const accountPublicKey = accountkeyPair.publicKey
 		const accountPrivateKey = accountkeyPair.privateKey
 
-		const blockKeyPair = await TrustNetEngine.generateSignatureKeyPair()
+		const blockKeyPair = await Core.generateSignatureKeyPair()
 		const blockPublicKey = blockKeyPair.publicKey
 		const blockPrivateKey = blockKeyPair.privateKey
 
 		const content = 'Hello world!'
 
-		const unsignedblockHeader = await TrustNetEngine.generateBlockHeader(
+		const unsignedblockHeader = await Core.generateBlockHeader(
 			content,
 			accountPublicKey,
 			blockPublicKey,
@@ -93,30 +87,26 @@ describe('Core module test suite', () => {
 			1,
 		)
 
-		const signedBlockHeader = await TrustNetEngine.signBlockHeader(
+		const signedBlockHeader = await Core.signBlockHeader(
 			unsignedblockHeader,
 			blockPrivateKey,
 		)
 
-		const unsignedSeedBlock = await TrustNetEngine.generateSeedBlock(
+		const unsignedSeedBlock = await Core.generateSeedBlock(
 			accountPublicKey,
 			blockPublicKey,
 			1,
 			1,
 		)
 
-		const signedSeedBlock = await TrustNetEngine.signSeedBlock(
+		const signedSeedBlock = await Core.signSeedBlock(
 			unsignedSeedBlock,
 			accountPrivateKey,
 		)
 
-		const blockMetadata = TrustNetEngine.generateMetadata(signedSeedBlock)
-		const block = TrustNetEngine.generateBlock(
-			signedBlockHeader,
-			content,
-			blockMetadata,
-		)
+		const blockMetadata = Core.generateMetadata(signedSeedBlock)
+		const block = Core.generateBlock(signedBlockHeader, content, blockMetadata)
 
-		expect(await TrustNetEngine.verifyBlock(block)).toEqual(true)
+		expect(await Core.verifyBlock(block)).toEqual(true)
 	})
 })
